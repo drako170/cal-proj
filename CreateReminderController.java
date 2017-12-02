c0a6c9bf2662a4e073d5af79541a1edc9894213b
drako170 <33813645+drako170@users.noreply.github.com>
2017-12-01 23:55:05 -0600

    Add files via upload

--- CreateReminderController.java
+++ CreateReminderController.java
@@ -11,15 +11,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.DatePicker;
+import javafx.stage.Window;
+import javafx.stage.Stage;
  
 public class CreateReminderController implements Initializable {
  
-   @FXML
-   private TextField monthData;
-  
-   @FXML
-   private TextField dayData;
-   
    @FXML
    private TextField timeData;
    
@@ -29,7 +26,11 @@ public class CreateReminderController implements Initializable {
    @FXML
    private Button createButton;
    
-
+   @FXML
+   private Button closeButton;
+   
+   @FXML
+   private DatePicker datePicker;
   
    @Override
    public void initialize(URL location, ResourceBundle resources) {
@@ -42,13 +43,30 @@ public class CreateReminderController implements Initializable {
    // this method will be called.
    // Creates new reminder object and adds it to static DList variable
    // Prints "Reminder Created!" to console
-   public void createReminder(ActionEvent event) {
-       System.out.println("Reminder Created!");
-      
-       Reminder newReminder = new Reminder(monthData.getText(),dayData.getText(),timeData.getText(),messageData.getText());
-       
-       ReminderApplication.list.addToLast(newReminder);
+   public void createReminder(ActionEvent event) throws IllegalStateException {
+	   try{    
+		   if(datePicker.getValue() == null){
+	    	   throw (new IllegalStateException("There is no data provided."));
+	       }
+	       
+		   System.out.println("Reminder Created!");
+	       
+	       Reminder newReminder = new Reminder(datePicker.getValue().getMonth().name(),Integer.toString(datePicker.getValue().getDayOfMonth()),timeData.getText(),messageData.getText());
+	       
+	       ReminderApplication.list.addToLast(newReminder);
+	       
+	       timeData.clear();
+	       messageData.clear();
+	       datePicker.getEditor().clear();
+	   }
+	   catch(IllegalStateException e){
+		   System.out.println(e.getMessage());
+	   }
    }
    
-  
+  public void closeButtonClicked(ActionEvent event){
+	  Stage stage = (Stage) closeButton.getScene().getWindow();
+	  stage.close();
+  }
+   
 }
